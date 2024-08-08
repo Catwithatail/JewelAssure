68129d5992f08cad33f90aa37303f930a9031f55
Aman Raj <amanraj3567@gmail.com>
2024-08-08 17:12:33 +0530

    front 1.0.1

--- client/app/page.js
+++ client/app/page.js
@@ -19,9 +19,28 @@ export default function Home() {
         />
       </div>
       <div className="gs">
-        <button>Get Started</button>
+        <button>Get Started </button>
       </div>
-      {/* <Footer /> */}
+      <div className="title2">Commitment to Confidence</div>
+      <div className="contain">
+        <div className="c1">
+          In an industry where authenticity and transparency are paramount,
+          JewelAssure is dedicated to transforming the jewelry market. We
+          understand the challenges consumers face with fake products, ethical
+          sourcing issues, and complex ownership verifications. Our mission is
+          to bring clarity and trust to your jewelry buying experience.
+        </div>
+        <div className="c2">
+          We provide verified information about the origin and craftsmanship of
+          each piece, ensuring you receive only genuine jewelry. Our platform
+          offers detailed insights into the sourcing and manufacturing
+          processes, helping you make informed and ethical purchasing decisions.
+          We streamline the process of verifying ownership and authenticity,
+          giving you confidence in your investment.
+        </div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
